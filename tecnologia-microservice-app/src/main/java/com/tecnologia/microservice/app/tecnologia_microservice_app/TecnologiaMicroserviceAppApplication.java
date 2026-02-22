9de40388a7cfaa5232041d599e6963d34cbd6b1b
Gabriel Infante <gabriel.infante@uptc.edu.co>
2026-02-22 15:11:19 -0500

    Se realizan configuracion inicial en el repositorio

---
+++ tecnologia-microservice-app/src/main/java/com/tecnologia/microservice/app/tecnologia_microservice_app/TecnologiaMicroserviceAppApplication.java
@@ -0,0 +1,13 @@
+package com.tecnologia.microservice.app.tecnologia_microservice_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecnologiaMicroserviceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecnologiaMicroserviceAppApplication.class, args);
+	}
+
+}
---
+++ tecnologia-microservice-app/src/test/java/com/tecnologia/microservice/app/tecnologia_microservice_app/TecnologiaMicroserviceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecnologia.microservice.app.tecnologia_microservice_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecnologiaMicroserviceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
